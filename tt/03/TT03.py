3d7bbaf469e8cc3fbbce76268a18a826246f8d4f
misrihtuni <miska.rihu@tuni.fi>
2025-10-31 15:44:56 +0200

    Add TT03

---
+++ tt/03/TT03.py
@@ -0,0 +1,15 @@
+###############################################################################
+# 4A00HB81 Python-ohjelmoitni (TAMK 2025)
+# =============================================================================
+# TT03.py
+#
+# Tämä esimerkki on tarkoitettu omatoimisen oppimisen tueksi ohjelmoinnin
+# opiskeluun. Muu käyttö kielletty.
+#
+###############################################################################
+
+teksti = "Python on tällä hetkellä mahdollisesti maailman suosituin ohjelmointikieli."
+print(teksti[:6] + '-' + teksti[58:69])
+
+###############################################################################
+# EOF
