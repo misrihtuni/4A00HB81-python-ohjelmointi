ec47f112e1b6e60e19c415beb49a183f3f7b4db0
misrihtuni <miska.rihu@tuni.fi>
2025-10-31 15:45:23 +0200

    Add TT04

---
+++ tt/04/TT04.py
@@ -0,0 +1,21 @@
+###############################################################################
+# 4A00HB81 Python-ohjelmoitni (TAMK 2025)
+# =============================================================================
+# TT04.py
+#
+# Tämä esimerkki on tarkoitettu omatoimisen oppimisen tueksi ohjelmoinnin
+# opiskeluun. Muu käyttö kielletty.
+#
+###############################################################################
+
+dna = 'ATATTGCTTGAAGCTGTACCGTTTTAGGGGGGGAACGCTGAAGATCTCTT'
+
+n_a = dna.count('A')
+n_c = dna.count('C')
+n_g = dna.count('G')
+n_t = dna.count('T')
+print(f'pituus = {len(dna)}')
+print(f'A: {n_a}  C: {n_c}  G: {n_g}  T: {n_t}')
+
+###############################################################################
+# EOF
