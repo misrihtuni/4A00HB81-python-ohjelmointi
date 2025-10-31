9c87c33f7c94ae28b49cf3f8c14a67d7c81f80fe
misrihtuni <miska.rihu@tuni.fi>
2025-10-31 15:46:02 +0200

    Add TT06

---
+++ tt/06/TT06a.py
@@ -0,0 +1,45 @@
+###############################################################################
+# 4A00HB81 Python-ohjelmoitni (TAMK 2025)
+# =============================================================================
+# TT06a.py
+#
+# Tämä esimerkki on tarkoitettu omatoimisen oppimisen tueksi ohjelmoinnin
+# opiskeluun. Muu käyttö kielletty.
+#
+###############################################################################
+
+k = int(input('Anna kuukauden numero (1 - 12): '))
+
+p = 0
+if k == 1:
+    p = 31
+elif k == 2:
+    p = 28  # HUOM! Ei tunnista karkausvuotta!
+elif k == 3:
+    p = 31
+elif k == 4:
+    p = 30
+elif k == 5:
+    p = 31
+elif k == 6:
+    p = 30
+elif k == 7:
+    p = 31
+elif k == 8:
+    p = 31
+elif k == 9:
+    p = 30
+elif k == 10:
+    p = 31
+elif k == 11:
+    p = 30
+elif k == 12:
+    p = 31
+
+if p != 0:
+    print(f'Kuukaudessa numero {k} on {p} päivää.')
+else:
+    print('Tarkista kuukauden numero!')
+
+###############################################################################
+# EOF
---
+++ tt/06/TT06b.py
@@ -0,0 +1,30 @@
+###############################################################################
+# 4A00HB81 Python-ohjelmoitni (TAMK 2025)
+# =============================================================================
+# TT06a.py
+#
+# Tämä esimerkki on tarkoitettu omatoimisen oppimisen tueksi ohjelmoinnin
+# opiskeluun. Muu käyttö kielletty.
+#
+###############################################################################
+
+k = int(input('Anna kuukauden numero (1 - 12): '))
+
+# kuukaudet 1, 3, 5, 7, 8, 10, 12: 31 päivää
+# kuukaudet 4, 6, 9, 11: 30 päivää
+# kuukausi 2: 28 päivää (tai 29)
+
+if 1 <= k <= 12:
+    if k == 1 or k == 3 or k == 5 or k == 7 or k == 8 or k == 10 or k == 12:
+        p = 31
+    elif k == 2:
+        p = 28  # HUOM! Ei tunnista karkausvuotta!
+    elif k == 4 or k == 6 or k == 9 or k == 11:
+        p = 30
+
+    print(f'Kuukaudessa numero {k} on {p} päivää.')
+else:
+    print(f'Kuukauden numero ei kelpaa!')
+
+###############################################################################
+# EOF
