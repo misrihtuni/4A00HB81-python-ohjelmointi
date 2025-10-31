67c7a93e5a5631df401b1096dafaae6f7cff5193
misrihtuni <miska.rihu@tuni.fi>
2025-10-31 12:52:05 +0200

    Add KT07

---
+++ kt/07/KT07.py
@@ -0,0 +1,77 @@
+﻿###############################################################################
+# 4A00HB81 Python-ohjelmoitni (TAMK 2025)
+# =============================================================================
+# KT07.py
+#
+# Opiskelija: Rihu Miska
+# Päiväys: 2025-10-31
+#
+###############################################################################
+# Tehtävänanto:
+#
+# Täydennä esimerkkinä käytettyä pizzalistaa (https://github.com/coniferprod/selpy-esim/blob/ee627cf34faf924be97b0fb75e7909293202cd7d/22/pizzat-taytteet.py)
+# siten, että lisäät sanakirjaan normaalikokoisen pizzan hinnan kullekin
+# pizzalle.
+#
+# Tee sitten Pythonilla pizzatilausohjelma, jossa kysytään montako pizzaa
+# tilataan ja minkälaisia. Jokaisen pizzan jälkeen tulosta välisumma. Lopuksi
+# tulosta pizzatilauksen kokonaishinta.
+#
+# Käyttäjän syötteet pitää tarkistaa, koska pizzalistassa on aukkoja, eli voi
+# olla sellaisia tilausnumeroita, joille ei löydy pizzaa. Käyttäjän ei pidä
+# voida tilata pizzaa, jonka tilausnumeroa ei löydy pizzalistasta, eikä hänen
+# myöskään pidä voida syöttää tilattavien pizzojen määränä negatiivista lukua
+# tai nollaa, eikä tilata yli 50 kappaletta mitään pizzaa.
+#
+# Esimerkki ohjelman käytöstä voisi näyttää vaikka tältä:
+#
+# Tervetuloa Guido's Pizza Palaceen, mitä haluaisitte?
+# Anna pizzan numero (0 = lopetus): 12
+# Pizzan numero 12 kappalemäärä: 2
+# Välisumma: 2 x 12.00 = 24.00 euroa
+# Anna pizzan numero (0 = lopetus): 1000
+# Virheellinen pizzan numero!
+# Anna pizzan numero (0 = lopetus): 19
+# Pizzan numero 19 kappalemäärä: 1
+# Välisumma: 1 x 13.00 = 13.00 euroa
+# Anna pizzan numero (0 = lopetus): 0
+# Kiitos tilauksestanne! Kokonaishintanne on 37.00 euroa.
+#
+###############################################################################
+
+# Sisällytetään pizzalistakirjastosta kaikki pizzat sisältävä sanakirja.
+from pizza_list import pizzas
+
+# Alustetaan loppusumma nollaksi.
+total = 0.0
+
+print("Tervetuloa Guido's Pizza Palaceen, mitä haluaisitte?")
+
+# Kysytään käyttäjältä pizzoja ja lukumääriä, kunnes käyttäjä päättää lopettaa.
+while True:
+    pizza_number = int(input("Anna pizzan numero (0 = lopetus): "))
+
+    # Poistutaan silmukasta, jos lopetusehto täyttyy.
+    if pizza_number == 0:
+        break
+
+    pizza_count = int(input(f"Pizzan numero {pizza_number} kappalemäärä: "))
+    # Haetaan pizza-sanakirja kaikkien pizzojen sanakirjasta.
+    pizza = pizzas[pizza_number]
+    # Etsitään pizza-sanakirjasta hinta-avaimen arvo.
+    pizza_price = pizza["price"]
+    # Lasketaan ja tulostetaan välisumma ja lisätään se loppusummaan.
+    subtotal = pizza_count * pizza_price
+    total += subtotal
+    print(f"Välisumma: {pizza_count} x {pizza_price:.2f} = {subtotal:.2f} euroa")
+
+if total != 0:
+    print(f"Kiitos tilauksestanne! Kokonaishintanne on {total:.2f} euroa.")
+else:
+    print(
+        "Kiitos käynnistä, toivottavasti löydätte ensi kerralla jotakin "
+        "mieluisaa tilattavaa."
+    )
+
+###############################################################################
+# EOF
---
+++ kt/07/pizza_list.py
@@ -0,0 +1,109 @@
+pizzas = {
+    1: {"name": "Bolognese", "toppings": ["jauheliha"], "price": 11.0},
+    2: {
+        "name": "Frutti di Mare",
+        "toppings": ["ananas", "katkarapu", "tonnikala"],
+        "price": 13.0,
+    },
+    3: {
+        "name": "Americano",
+        "toppings": ["ananas", "aurajuusto", "kinkku"],
+        "price": 15.0,
+    },
+    4: {
+        "name": "Opera Special",
+        "toppings": ["kinkku", "salami", "tonnikala"],
+        "price": 13.5,
+    },
+    5: {
+        "name": "Mexicana",
+        "toppings": ["ananas", "pepperoni", "chili", "tacokastike"],
+        "price": 15.0,
+    },
+    6: {
+        "name": "Julia",
+        "toppings": ["ananas", "aurajuusto", "katkarapu", "kinkku"],
+        "price": 15.5,
+    },
+    7: {
+        "name": "Empire Special",
+        "toppings": [
+            "katkarapu",
+            "kinkku",
+            "mustapippuri",
+            "salami",
+            "sipuli",
+            "tuplajuusto",
+            "valkosipuli",
+        ],
+        "price": 16.0,
+    },
+    8: {
+        "name": "Kummisetä",
+        "toppings": ["herkkusieni", "katkarapu", "kinkku", "valkosipuli"],
+        "price": 14.0,
+    },
+    9: {
+        "name": "Chicken Hawaii",
+        "toppings": ["ananas", "aurajuusto", "kana"],
+        "price": 13.5,
+    },
+    11: {
+        "name": "Romeo",
+        "toppings": ["ananas", "aurajuusto", "katkarapu", "salami"],
+        "price": 14.5,
+    },
+    12: {
+        "name": "Vegetariana",
+        "toppings": ["herkkusieni", "oliivi", "paprika", "sipuli", "tomaatti"],
+        "price": 13.0,
+    },
+    13: {
+        "name": "Dillinger",
+        "toppings": ["jauheliha", "kinkku", "salami", "sipuli"],
+        "price": 14.0,
+    },
+    14: {
+        "name": "Papa" "s Special",
+        "toppings": [
+            "aurajuusto",
+            "mustapippuri",
+            "oliivi",
+            "paprika",
+            "salami",
+            "sipuli",
+        ],
+        "price": 14.5,
+    },
+    15: {
+        "name": "Quattro Stagioni",
+        "toppings": ["herkkusieni", "katkarapu", "kinkku", "paprika"],
+        "price": 13.5,
+    },
+    16: {
+        "name": "Cambretti",
+        "toppings": ["tuplajuusto", "katkarapu", "valkosipuli"],
+        "price": 13.0,
+    },
+    17: {
+        "name": "Pepperoni",
+        "toppings": ["paprika", "pepperoni", "tonnikala"],
+        "price": 13.0,
+    },
+    19: {
+        "name": "Spicy Hot Crispy",
+        "toppings": ["mausteinen naudanliha", "sipuli", "tomaatti", "chili"],
+        "price": 14.0,
+    },
+    21: {
+        "name": "Finlandia",
+        "toppings": ["kana", "katkarapu", "kinkku", "salami", "tonnikala"],
+        "price": 16.0,
+    },
+    23: {
+        "name": "Driver" "s Special",
+        "toppings": ["pekoni", "pepperoni", "kinkku", "salami", "aurajuusto"],
+        "price": 16.5,
+    },
+    26: {"name": "Fantasia", "toppings": [None, None, None, None], "price": 13.0},
+}
