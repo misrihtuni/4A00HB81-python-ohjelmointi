a5ee2094267ae7ffd064f29c24f9a121204cc67e
misrihtuni <miska.rihu@tuni.fi>
2025-10-31 15:44:33 +0200

    Add TT01

---
+++ tt/01/TT01b.py
@@ -0,0 +1,15 @@
+###############################################################################
+# 4A00HB81 Python-ohjelmoitni (TAMK 2025)
+# =============================================================================
+# TT01b.py
+#
+# Tämä esimerkki on tarkoitettu omatoimisen oppimisen tueksi ohjelmoinnin
+# opiskeluun. Muu käyttö kielletty.
+#
+###############################################################################
+
+ika = int(input('Anna ikäsi: '))
+print(f'50 vuoden kuluttua olet {ika + 50}-vuotias.')
+
+###############################################################################
+# EOF
