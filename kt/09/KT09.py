1d1e9b8f424d4216bf88f4beabfee68c3ba2791e
misrihtuni <miska.rihu@tuni.fi>
2025-11-14 14:04:57 +0200

    Add KT09

--- kt/09/KT09.py
+++ kt/09/KT09.py
@@ -4,7 +4,7 @@
 # KT09.py
 #
 # Opiskelija: Rihu Miska
-# Päiväys: 2025-mm-dd
+# Päiväys: 2025-11-14
 #
 ###############################################################################
 # Tehtävänanto:
@@ -13,6 +13,48 @@
 #
 ###############################################################################
 
+from KT09_Pizza import Pizza
+
+pizza_dict = {
+    1: Pizza("Margherita", ["mozzarella", "tomaatti"]),
+    2: Pizza("Bolognese", ["jauhelihakastike"]),
+    3: Pizza("Americana", ["kinkku", "ananas", "aurajuusto"]),
+}
+
+# Lisätään pizzoja listaan
+pizza_dict[1].set_prices(10.5, 18, 15)
+pizza_dict[2].set_prices(11, 18, 15)
+pizza_dict[3].set_prices(11, 20, 16)
+
+
+# Tulostaa listan yhden rivin.
+def print_menu_row(number, name, price_normal, price_family, price_pan):
+    print(
+        f"{number:3}"
+        f" {name:30}"
+        f" {price_normal:^8}"
+        f" {price_family:^8}"
+        f" {price_pan:^8}"
+    )
+
+
+# Tulostaa ruokalistan.
+def print_menu(pizza_dict):
+    print_menu_row("Nro", "Nimi", "Normaali", "Perhe", "Pannu")
+    for number, pizza in pizza_dict.items():
+        prices = pizza.get_prices()
+        print_menu_row(
+            number,
+            pizza.name,
+            f"{prices[0]:.2f}",
+            f"{prices[1]:.2f}",
+            f"{prices[2]:.2f}",
+        )
+        print_menu_row("", pizza.get_toppings(), "", "", "")
+        print()
+
+
+print_menu(pizza_dict)
 
 ###############################################################################
 # EOF
---
+++ kt/09/KT09_Pizza.py
@@ -0,0 +1,49 @@
+﻿###############################################################################
+# 4A00HB81 Python-ohjelmoitni (TAMK 2025)
+# =============================================================================
+# KT09_Pizza.py
+#
+# Opiskelija: Rihu Miska
+# Päiväys: 2025-11-14
+#
+###############################################################################
+
+
+class Pizza:
+    name: str
+    _toppings: list[str]
+    _prices: dict[str, float]
+
+    def __init__(self, name: str, toppings: list[str]):
+        self.name = name
+        self._toppings = toppings
+        self._prices = {"normal": 0.0, "family": 0.0, "pan": 0.0}
+
+    def get_toppings(self) -> str:
+        toppings_str = ""
+        for topping in self._toppings[:-1]:
+            toppings_str += f"{topping}, "
+        toppings_str += f"{self._toppings[-1]}"
+        return toppings_str
+
+    def has_topping(self, topping: str) -> bool:
+        return topping in self._toppings
+
+    def set_prices(
+        self,
+        price_normal: float | int,
+        price_family: float | int,
+        price_pan: float | int,
+    ) -> None:
+        self._prices["normal"] = float(price_normal)
+        self._prices["family"] = float(price_family)
+        self._prices["pan"] = float(price_pan)
+
+    def get_prices(self) -> tuple[float, ...]:
+        return tuple(
+            [self._prices["normal"], self._prices["family"], self._prices["pan"]]
+        )
+
+
+###############################################################################
+# EOF
