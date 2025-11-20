b8054a4bb18f54510ae8606a59b29979b9d78f27
misrihtuni <miska.rihu@tuni.fi>
2025-11-20 13:53:15 +0200

    Add KT10

--- kt/10/KT10.py
+++ kt/10/KT10.py
@@ -4,14 +4,74 @@
 # KT10.py
 #
 # Opiskelija: Rihu Miska
-# Päiväys: 2025-mm-dd
+# Päiväys: 2025-11-20
 #
 ###############################################################################
 # Tehtävänanto:
 #
-# teksti tähän
+# Tee Python-moduuli nimeltä paikat, joka sisältää määrityksen luokalle nimeltä
+# Paikka. Luokan tietojäseninä ovat paikan nimi ja kuvaus (molemmat
+# merkkijonoja).
+#
+# Tee sitten testiohjelma, joka lukee kaikki paikat JSON-muotoisesta
+# tiedostosta nimeltä paikat.json, ja tekee niistä Paikka-olioita. (Tätä varten
+# pitää tuoda Paikka-luokka yllä olevan mukaisesti tehdystä paikat-moduulista.)
+#
+# JSON-tiedostossa paikat ovat seuraavassa muodossa:
+#
+# [
+#     {"nimi": "Sisäpiha", "kuvaus": "TAMKin Teiskontien puoleinen sisäpiha."},
+#     {"nimi": "Teiskontien aula", "kuvaus": "TAMKin Teiskontien aula."},
+#     {"nimi": "Palvelukatu", "kuvaus": "Palvelukatu. Tämän käytävän varrella sijaitsevat opintopalvelut."},
+#     {"nimi": "Tietotalon aula", "kuvaus": "Aulatila, josta pääsee C-taloon ja juhlasaliin."},
+#     {"nimi": "Juhlasali", "kuvaus": "TAMKin juhlasali."},
+#     {"nimi": "Kuntokadu aula", "kuvaus": "Kuntokadun puoleinen eteisaula."}
+# ]
+#
+# Ole tarkkana JSON-tiedoston kanssa. Se sisältää taulukon, joka vastaa
+# Pythonin listaa. Taulukon sisällä on olioita, jotka vastaavat Pythonin
+# sanakirjoja. Paikka-oliot tehdään hakemalla näistä sanakirjoista arvot
+# käyttämällä avaimina luokan tietojäsenien nimiä.
+#
+# JSON-tiedoston voi lukea käyttämällä json-moduulin load-funktiota, jolle
+# annetaan lukemista varten avatun tiedoston nimi. Tarkista parametrit funktion
+# dokumentaatiosta. Kannattaa käyttää with-käskyä tiedoston avaamiseen, niin ei
+# tarvitse huolehtia tiedoston sulkemisesta.
+#
+# Tulosta lopuksi lista paikoista muodossa "nimi: kuvaus".
 #
 ###############################################################################
+
+import json
+
+from KT10_places import Place
+
+
+def read_places_from_file(filepath: str) -> list[Place]:
+    """Reads a JSON file containing a list of places.
+
+    The file should be in format [{"name": str, "description": str},...]
+    :param filepath: path to the JSON file to read
+    :return: list of Place objects
+    """
+    places: list[Place] = []
+    with open(filepath, "r", encoding="utf-8") as f:
+        json_content = json.load(f)
+        for item in json_content:
+            places.append(Place(item["name"], item["description"]))
+    return places.copy()
+
+
+def print_places(places: list[Place]) -> None:
+    """Prints the places in the given list.
+    :param places: list of Place objects to print
+    """
+    for place in places:
+        print(place)
+
+
+places = read_places_from_file("KT10_places.json")
+print_places(places)
 
 
 ###############################################################################
---
+++ kt/10/KT10_places.py
@@ -0,0 +1,44 @@
+﻿###############################################################################
+# 4A00HB81 Python-ohjelmointi (TAMK 2025)
+# =============================================================================
+# KT10_places.py
+#
+# Opiskelija: Rihu Miska
+# Päiväys: 2025-11-20
+#
+###############################################################################
+
+
+class Place:
+    """Represents a place object in the game.
+
+    Contains the name and the description of the place and has methods for
+    setting and retrieving those attributes.
+    """
+
+    _name: str
+    _description: str
+
+    def __init__(self, name: str, description: str):
+        self._name = name
+        self._description = description
+
+    def get_name(self) -> str:
+        """Returns the name of the place."""
+        return self._name
+
+    def set_description(self, description: str) -> None:
+        """Sets the description of the place to the given text."""
+        self._description = description.strip()
+
+    def get_description(self) -> str:
+        """Returns the description of the place as text."""
+        return self._description
+
+    def __str__(self):
+        """Returns the place as text in the format <name>: <description>"""
+        return f"{self.get_name()}: {self.get_description()}"
+
+
+###############################################################################
+# EOF
